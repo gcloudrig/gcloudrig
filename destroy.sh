a602e60bc26988d4d41776bf8855e01bce98ab5e
putty182 <putty182@gmail.com>
2018-11-22 03:13:32 +1000

    refactor + bugfixes

--- destroy.sh
+++ destroy.sh
@@ -1,27 +1,28 @@
 #!/usr/bin/env bash
 
-# gcloudrig/destroy.sh
-
 # exit on error
 set -e
 
-# load globals
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
-source $DIR/globals.sh
+# full path to script dir
+DIR="$( cd "$( dirname -- "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+
+# load globals 
+# shellcheck source=globals.sh
+source "$DIR/globals.sh"
 
 # shut it down
 gcloudrig_stop || echo .
 
 # delete managed instance group
-gcloud compute instance-groups managed delete $INSTANCEGROUP --region $REGION --quiet || echo .
+gcloud compute instance-groups managed delete "$INSTANCEGROUP" --region "$REGION" --quiet || echo .
 
 # delete instance templates
-gcloud compute instance-templates delete $INSTANCETEMPLATE-base --quiet || echo .
-gcloud compute instance-templates delete $INSTANCETEMPLATE --quiet || echo .
+gcloud compute instance-templates delete "$INSTANCETEMPLATE-base" --quiet || echo .
+gcloud compute instance-templates delete "$INSTANCETEMPLATE" --quiet || echo .
 
 # delete disks
-gcloud compute disks delete $GAMESDISK --zone $ZONE --quiet || echo .
-gcloud compute disks delete $BOOTDISK --zone $ZONE --quiet || echo .
+gcloud compute disks delete "$GAMESDISK" --zone "$ZONE" --quiet || echo .
+gcloud compute disks delete "$BOOTDISK" --zone "$ZONE" --quiet || echo .
 
 # delete image
 gcloud compute images delete $IMAGE --quiet || echo .
