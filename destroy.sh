5a665e509af35c62d3ab73deec32ee6ef5500255
putty182 <putty182>
2018-11-22 21:19:33 +1000

    Tidy up console output

--- destroy.sh
+++ destroy.sh
@@ -6,23 +6,17 @@ set -e
 # full path to script dir
 DIR="$( cd "$( dirname -- "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
-# load globals 
+# load globals
 # shellcheck source=globals.sh
 source "$DIR/globals.sh"
+init_globals;
 
 # shut it down
-gcloudrig_stop || echo .
+gcloudrig_stop || echo -n
 
 # delete managed instance group
-gcloud compute instance-groups managed delete "$INSTANCEGROUP" --region "$REGION" --quiet || echo .
+gcloud compute instance-groups managed delete "$INSTANCEGROUP" --region "$REGION" --quiet || echo -n
 
 # delete instance templates
-gcloud compute instance-templates delete "$INSTANCETEMPLATE-base" --quiet || echo .
-gcloud compute instance-templates delete "$INSTANCETEMPLATE" --quiet || echo .
-
-# delete disks
-gcloud compute disks delete "$GAMESDISK" --zone "$ZONE" --quiet || echo .
-gcloud compute disks delete "$BOOTDISK" --zone "$ZONE" --quiet || echo .
-
-# delete image
-gcloud compute images delete $IMAGE --quiet || echo .
+gcloud compute instance-templates delete "$INSTANCETEMPLATE-base" --quiet || echo -n
+gcloud compute instance-templates delete "$INSTANCETEMPLATE" --quiet || echo -n
