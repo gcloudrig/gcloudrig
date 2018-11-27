b1cc619665b2904dea5c50f069cad6c0ac2f2b1f
putty182 <putty182@gmail.com>
2018-11-27 17:44:40 +1000

    - added check for logging.googleapis.com - removed `sed` hacks - tidied up instance group/template lifecycle

--- setup.sh
+++ setup.sh
@@ -13,85 +13,56 @@ DIR="$( cd "$( dirname -- "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 source "$DIR/globals.sh"
 init_setup # init;
 
-# create/recreate instance group and template
+# # create/recreate instance group; uses the startup template by default
 gcloudrig_delete_instance_group
-gcloudrig_delete_instance_template
-gcloudrig_create_instance_template
 gcloudrig_create_instance_group
 
-# create gcs bucket
-GCSBUCKET="gs://$PROJECT_ID"
+OPTIONS="Yes No"
+echo "Would you like to automatically install some things?"
+select opt in $OPTIONS; do
+    if [ "$opt" == "Yes" ]; then
+        # create GCS bucket and upload script
+        echo "Creating GCS bucket $GCSBUCKET/ to store install script..."
+        gsutil mb -p "$PROJECT_ID" -c regional -l "$REGION"  "$GCSBUCKET/" || echo "already exists?"
 
-echo "Creating GCS bucket $GCSBUCKET/ to store install script..."
-gsutil mb -p "$PROJECT_ID" -c regional -l "$REGION"  "$GCSBUCKET/" || echo "already exists?"
+        echo "Copying software install script to GCS..."
+        gsutil cp "$DIR/gcloudrig.psm1" "$GCSBUCKET/"
 
-echo "Copying software install script to GCS..."
-gsutil cp "$DIR/gcloudrig.psm1" "$GCSBUCKET/"
+        # announce script's gcs url via project metadata
+        gcloud compute project-info add-metadata --metadata "gcloudrig-setup-script-gcs-url=$GCSBUCKET/gcloudrig.psm1" --quiet
 
-# replace any '#' chars, since they're used in sed command
-# TODO: This is horribly insecure; migrate to `gcloud kms` instead?
-WINDOWS_PASS="$(generate_windows_password | tr '#' '^')"
+        # start 'er up
+        gcloudrig_start
+        gcloudrig_mount_games_disk
 
-# create installation template and point group at it
-echo "Creating setup template $SETUPTEMPLATE..."
-gcloud compute instance-templates create "$SETUPTEMPLATE" \
-    --accelerator "type=$ACCELERATORTYPE,count=$ACCELERATORCOUNT" \
-    --boot-disk-type "$BOOTTYPE" \
-    --image-family "$IMAGEBASEFAMILY" \
-    --image-project "$IMAGEBASEPROJECT" \
-    --labels "$GCRLABEL=true" \
-    --machine-type "$INSTANCETYPE" \
-    --maintenance-policy "TERMINATE" \
-    --no-boot-disk-auto-delete \
-    --no-restart-on-failure \
-    --format "value(name)" \
-    --metadata-from-file windows-startup-script-ps1=<(sed -e "s#@URL@#$GCSBUCKET/gcloudrig.psm1#;s#@PASSWORD@#$WINDOWS_PASS#" "$DIR/windows-setup.ps1.template") \
-    --quiet
+        # wait until script is complete
+        logURL="https://console.cloud.google.com/logs/viewer?authuser=1&project=${PROJECT_ID}&resource=global&logName=projects%2F${PROJECT_ID}%2Flogs%2Fgcloudrig-install"
+        echo "Software install will take a while. Watch the logs at:"
+        echo
+        echo "  $logURL"
+        echo
+        read -r -p "Press enter when software install is complete..."
 
-# point group at installation template
-gcloud compute instance-groups managed "$INSTANCEGROUP" set-instance-template "$SETUPTEMPLATE"
+        echo "Removing software install script from GCS..."
+        gsutil rm "$GCSBUCKET/gcloudrig.psm1"
 
-# start 'er up
-gcloudrig_start
-gcloudrig_mount_games_disk
+        # announce script's gcs url via project metadata
+        gcloud compute project-info remove-metadata --keys "gcloudrig-setup-script-gcs-url,gcloudrig-setup-script-finished" --quiet
 
-# wait until script is complete
-logURL="https://console.cloud.google.com/logs/viewer?authuser=1&project=${PROJECT_ID}&resource=global&logName=projects%2F${PROJECT_ID}%2Flogs%2Fgcloudrig-install"
-echo "Software install will take a while. Watch the logs at:"
-echo
-echo "  $logURL"
-echo
-echo "The last line will be 'All done!'"
-echo
-echo "To connect with RDP"
-echo "  username: gcloudrig"
-echo "  password: $WINDOWS_PASS"
-read -r -p "Press enter when software install is complete..."
+        # shut it down
+        gcloudrig_stop
 
-echo "Removing software install script from GCS..."
-gsutil rm "$GCSBUCKET/gcloudrig.psm1"
+        # save boot image (in the background)
+        gcloudrig_boot_disk_to_image &
 
-# shut it down
-gcloudrig_stop
+        # save games snapshot (in the background)
+        gcloudrig_games_disk_to_snapshot &
 
-# save boot image (in the background)
-gcloudrig_boot_disk_to_image &
-
-# save games snapshot (in the background)
-gcloudrig_games_disk_to_snapshot &
-
-# point managed instance group back at the real template
-echo "Tidying up..."
-gcloud compute instance-groups managed set-instance-template "$INSTANCEGROUP" \
-    --template "$INSTANCETEMPLATE" \
-    --region "$REGION" \
-    --quiet
-
-# delete setup template
-gcloud compute instance-templates delete "$SETUPTEMPLATE" \
-    --quiet
-
-# wait for background tasks to complete
-wait
+        # wait for background tasks to complete
+        wait
+    elif [ "$opt" == "No" ]; then
+        break
+    fi
+done
 
 echo "Done!  Run './scale-up.sh' to start your instance."
--- windows-setup.ps1
+++ windows-setup.ps1
@@ -6,17 +6,33 @@ if (Test-Path "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp\gclo
 } else {
 
   gcloud logging write gcloudrig-install ".download gcloudrig.psm1 from GCS"
+
+  $SetupScriptUrl=Get-GceMetadata -Path "gcloudrig/setup-script-gcs-url"
+  $InstanceName=Get-GceMetadata -Path "name"
+  $InstanceZone=Get-GceMetadata -Path "zone"
+
   # download installer module from GCS
-  & gsutil cp "@URL@" "$Home\Desktop\gcloudrig.psm1" | Out-File "c:\gcloudrig-setup.txt" -Append
+  & gsutil cp "$SetupScriptUrl" "$Home\Desktop\gcloudrig.psm1" | Out-File "c:\gcloudrig-setup.txt" -Append
   if (Test-Path "$Home\Desktop\gcloudrig.psm1") {
     New-Item -ItemType directory -Path "$Env:ProgramFiles\WindowsPowerShell\Modules\gCloudRig" -Force
     Copy-Item "$Home\Desktop\gcloudrig.psm1" -Destination "$Env:ProgramFiles\WindowsPowerShell\Modules\gCloudRig\" -Force
-
     Import-Module gCloudRig
-    New-gCloudRigInstall -Password "@PASSWORD@"
+
+    # create a new account and password
+    $Password=gcloud compute reset-windows-password "$InstanceName" --user "gcloudrig" --zone "$InstanceZone" --format "value(password)"
+
+    # TODO: put this somewhere safer
+    gcloud logging write gcloudrig-install ".user account created/reset; username:gcloudrig; password:$Password"
+
+    New-gCloudRigInstall -Password "$Password"
+
   } else {
     gcloud logging write gcloudrig-install --severity ERROR ".download of gcloudrig.psm1 failed!"
   }
 }
 
+# announce that we're done
 gcloud logging write gcloudrig-install "windows-setup.ps1 finished"
+
+gcloud compute instances add-metadata "$InstanceName" --metadata "gcloudrig-setup-script-finished=true"
+
