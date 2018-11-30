e564daa007e5cf2ecfd705d13694647b1452f2af
putty182 <putty182@gmail.com>
2018-12-01 07:01:02 +1000

    Rewrite gcloudrig-boot.ps1

--- gcloudrig-boot.ps1
+++ gcloudrig-boot.ps1
@@ -1,23 +1,41 @@
-$GAMESDISK="gcloudrig-games"
-$GCRLABEL="gcloudrig"
+$GCRLABEL, $GamesDiskName, $ZoneName, $InstanceName, $Instance, $GamesDisk, $LatestSnapshotName, $Snapshot
 
-$PROJECT=(Get-GceMetadata -Path "project/project-id")
-$INSTANCE=(Get-GceMetadata -Path "instance/name")
-$ZONE=(Get-GceMetadata -Path "instance/zone" | Split-Path -Leaf)
+# restore/create games disk and mounts it if it's not already attached somewhere
+function MountGamesDisk {
+    If (-Not $GamesDisk) {
+        If ($Snapshot) {
+            Write-Output "Restoring games disk..."
+            $GamesDisk=(New-GceDisk -DiskName "$GamesDiskName" -Snapshot $Snapshot -Zone "$ZoneName")
+        } Else {
+            Write-Output "Creating blank games disk..."
+            $GamesDisk=(New-GceDisk -DiskName "$GamesDiskName" -Zone "$ZoneName")
+        }
+    }
 
-
-$snapshot=(gcloud compute snapshots list --format "value(name)" --filter "labels.gcloudrig=true labels.latest=true" --project=$PROJECT)
-$existingdisk=(gcloud compute disks list --filter "name=$GAMESDISK zone:($ZONE)" --format "value(name)")
-
-# create a blank games disk
-If (-Not $snapshot -And -Not $existingdisk) {
-    echo "Creating a blank games disk..."
-    gcloud compute disks create "$GAMESDISK" --zone "$ZONE" --quiet --labels "$GCRLABEL=true"
-} ElseIf (-Not $existingdisk) {
-    echo "Restoring games disk from snapshot $snapshot..."
-    gcloud compute disks create "$GAMESDISK" --zone "$ZONE" --quiet --labels "$GCRLABEL=true" --source-snapshot "$snapshot"
+    If ($GamesDisk.Users -And ($GamesDisk.Users | Split-Path -Leaf) -Eq $Instance.Name) {
+        Write-Output "Games Disk is already attached!"
+    } Else {
+        Write-Output "Mounting games disk..."
+        Set-GceInstance $Instance -AddDisk $GamesDisk
+    }
 }
 
-# disk now exists, so attach it
-echo "Mounting games disk..."
-gcloud compute instances attach-disk "$INSTANCE" --disk "$GAMESDISK" --zone "$ZONE" --quiet
+function BootCompleted {
+    Set-GceInstance $Instance -AddMetadata @{ "gcloudrig-boot" = "true"; }
+    Restart-Computer -Force -Wait
+}
+
+# business time
+if (-Not (Get-GceMetadata -Path "instance/attributes/gcloudrig-boot") -Eq "true") {
+    $GCRLABEL="gcloudrig"
+    $GamesDiskName="gcloudrig-games"
+    $ZoneName=(Get-GceMetadata -Path "instance/zone" | Split-Path -Leaf)
+    $InstanceName=(Get-GceMetadata -Path "instance/name")
+    $Instance=(Get-GceInstance $InstanceName -Zone "$ZoneName")
+    $GamesDisk=(Get-GceDisk -DiskName "$GamesDiskName")
+    $LatestSnapshotName=(gcloud compute snapshots list --format "value(name)" --filter "labels.$GCRLABEL=true labels.latest=true" --project (Get-GceMetadata -Path "project/project-id"))
+    $Snapshot=(Get-GceSnapshot -Name "$LatestSnapshotName")
+
+    MountGamesDisk
+    BootCompleted
+}
