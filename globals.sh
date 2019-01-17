7ebde0f7057474a92ca8a886bb85651720131cc4
Daniel Quinlan <daniel@chaosengine.net>
2019-01-16 22:20:13 -0800

    move quota functions in globals.sh to avoid merge conflict

--- globals.sh
+++ globals.sh
@@ -292,8 +292,39 @@ function gcloudrig_delete_instance_group {
   done
 }
 
+##################
+# QUOTA COMMANDS #
+##################
 
+function gcloudrig_get_project_quota_limits {
+  declare -gA QUOTAS
+  for line in $(gcloud compute project-info describe \
+    --project="$PROJECT_ID" \
+    --flatten="quotas[]" \
+    --format="csv[no-heading](quotas.metric,quotas.limit)") ; do
+    QUOTAS[${line%%,*}]="${line##*,}"
+  done
+}
 
+function gcloudrig_check_quota_gpus_all_regions {
+  if [ ! -v QUOTAS ] ; then
+    gcloudrig_get_project_quota_limits
+  fi
+
+  # if key exists in array
+  if [ -v "QUOTAS[GPUS_ALL_REGIONS]" ] ; then
+    # gcloud --format option sometimes outputs nothing if the value is 0.0
+    if [ -z "${QUOTAS[GPUS_ALL_REGIONS]}" -o "${QUOTAS[GPUS_ALL_REGIONS]}" == "0.0" ] ; then
+      echo "You have to manually request a quota increase for GPUS_ALL_REGIONS" >&2
+      echo "See https://cloud.google.com/compute/quotas#requesting_additional_quota" >&2
+      exit 1
+    else
+      echo "GPUS_ALL_REGIONS quota is ${QUOTAS["GPUS_ALL_REGIONS"]}"
+    fi
+  else
+    echo "no GPUS_ALL_REGIONS quota, good to go."
+  fi
+}
 
 ##################
 # OTHER COMMANDS #
@@ -516,34 +547,3 @@ function gcloudrig_mount_games_disk {
     --zone "$ZONE" \
     --quiet &>/dev/null
 }
-
-function gcloudrig_get_project_quota_limits {
-  declare -gA QUOTAS
-  for line in $(gcloud compute project-info describe \
-    --project="$PROJECT_ID" \
-    --flatten="quotas[]" \
-    --format="csv[no-heading](quotas.metric,quotas.limit)") ; do
-    QUOTAS[${line%%,*}]="${line##*,}"
-  done
-}
-
-function gcloudrig_check_quota_gpus_all_regions {
-  if [ ! -v QUOTAS ] ; then
-    gcloudrig_get_project_quota_limits
-  fi
-
-  # if key exists in array
-  if [ -v "QUOTAS[GPUS_ALL_REGIONS]" ] ; then
-    # gcloud --format option sometimes outputs nothing if the value is 0.0
-    if [ -z "${QUOTAS[GPUS_ALL_REGIONS]}" -o "${QUOTAS[GPUS_ALL_REGIONS]}" == "0.0" ] ; then
-      echo "You have to manually request a quota increase for GPUS_ALL_REGIONS" >&2
-      echo "See https://cloud.google.com/compute/quotas#requesting_additional_quota" >&2
-      exit 1
-    else
-      echo "GPUS_ALL_REGIONS quota is ${QUOTAS["GPUS_ALL_REGIONS"]}"
-    fi
-  else
-    echo "no GPUS_ALL_REGIONS quota, good to go."
-  fi
-}
-
