2fe116a5658614d7ee30aab87cc52bc52ff014d1
Daniel Quinlan <daniel@chaosengine.net>
2019-02-17 10:42:41 -0800

    yay, shell quoting

--- globals.sh
+++ globals.sh
@@ -421,7 +421,7 @@ function gcloudrig_enable_software_setup {
 
 # TODO store the password somewhere safer
 function gcloudrig_get_password_from_logs {
-  gcloud logging read 'logName=projects/$PROJECT_ID/logs/gcloudrig-install AND textPayload:password' --format="value(textPayload)" --limit=1
+  gcloud logging read "logName=projects/$PROJECT_ID/logs/gcloudrig-install AND textPayload:password" --format="value(textPayload)" --limit=1
 }
 
 # create GCS bucket, don't fail if it already exists
