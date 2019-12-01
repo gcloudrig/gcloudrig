c21e9b9704494be9f34d21904784958acb101e4e
putty182 <putty182>
2019-12-01 12:03:53 +1000

    update log url

--- globals.sh
+++ globals.sh
@@ -663,7 +663,7 @@ function gcloudrig_start {
   ZONE="$(gcloudrig_get_instance_zone_from_group "$INSTANCEGROUP")"
   BOOTDISK="$(gcloudrig_get_bootdisk_from_instance "$ZONE" "$INSTANCE")"
 
-  echo "To watch boot/setup progress, visit `https://console.cloud.google.com/logs/viewer?project=$PROJECT_ID&advancedFilter=logName%3D%22projects%2F$PROJECT_ID%2Flogs%2Fgcloudrig-install%22`"
+  echo "To watch boot/setup progress, visit `https://console.cloud.google.com/logs/viewer?project=$PROJECT_ID&advancedFilter=logName:%22projects%2F$PROJECT_ID%2Flogs`"
 }
 
 # scale to 0 and wait
