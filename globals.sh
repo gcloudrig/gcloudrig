22149bc4a5ed01bf69307a61fe010196efcd9731
putty182 <putty182@gmail.com>
2020-12-22 23:24:37 +1000

    Update globals.sh

--- globals.sh
+++ globals.sh
@@ -38,6 +38,8 @@ SETUPOPTIONS[DisplayScaling]=""
 SETUPOPTIONS[InstallSteam]="false"
 SETUPOPTIONS[InstallBattlenet]="false"
 SETUPOPTIONS[InstallSSH]="false"
+SETUPOPTIONS[InstallGoogleChrome]="false"
+SETUPOPTIONS[InstallFirefox]="false"
 
 ########
 # INIT #
