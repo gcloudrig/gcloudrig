a50fcf00c66efdf4e469eb675c390dc843634b74
putty182 <putty182@gmail.com>
2021-01-27 13:12:28 +0000

    reduce scale-up timeout from 300s to 120s

--- globals.sh
+++ globals.sh
@@ -24,7 +24,7 @@ DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
 
 # emergency debug
 set -e; [ -n "$GCLOUDRIG_DEBUG" ] && set -x
-source "$DIR/config.sh"
+source "$DIR/config.sh"; popd
 ##############################################################
 
 ########
